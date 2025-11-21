a7adc1d565afac01eb5e44cc770be28f18003195
MArebato79 <miguelrebatovalero@gmail.com>
2025-11-21 22:11:07 +0100

    Delete ProyectoReservas directory

--- ProyectoReservas/src/main/java/com/example/ProyectoReservas/entities/diaSemana.java
+++
@@ -1,5 +0,0 @@
-package com.example.ProyectoReservas.entities;
-
-public enum diaSemana{
-    LUNES,MARTES,MIERCOLES,JUEVES,VIERNES
-}
--- ProyectoReservas/src/main/java/com/example/ProyectoReservas/entities/tipoHorario.java
+++
@@ -1,5 +0,0 @@
-package com.example.ProyectoReservas.entities;
-
-public enum tipoHorario{
-    LECTIVO,RECREO,MEDIO_DIA
-}
